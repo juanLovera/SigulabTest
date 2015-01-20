64522a20589c344a4e9269c6efffc5299e113dfd
rosangelisgarcia <rosangelis.92@gmail.com>
2015-01-20 14:12:38 -0430

    Agregar monto ejecutado en 'Resumen Presupuestario'

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -11,15 +11,26 @@ class BudgetController < ApplicationController
     @incomes = Income.all
     @commitments = Commitment.all
 
+    @executions = Execution.all
+
     @incomes_lab = []
     @commitments_lab = []
+
+    @executions_commitement = []
+
     @incomes_total = 0
     @commitments_total = 0
+
+    @executions_total = 0
+
+
     @labs.each do |l|
       @incomes_lab[l.id] = @incomes.where(lab_id: l.id).sum(:amount)
       @incomes_total += @incomes_lab[l.id]
       @commitments_lab[l.id] = @commitments.where(lab_id: l.id).sum(:amount)
       @commitments_total += @commitments_lab[l.id]
+      @executions_commitement[l.id] = @executions.where(commitment_id: l.id).sum(:check_amount)
+      @executions_total += @executions_commitement[l.id]
     end
   end
   
