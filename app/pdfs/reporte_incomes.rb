b72a2b50fd3b2046be5fffb30bfe91bcf0a68354
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2015-01-21 18:01:32 -0430

    PDF

--- app/pdfs/reporte_incomes.rb
+++ app/pdfs/reporte_incomes.rb
@@ -11,9 +11,9 @@ class ReporteIncomes < Prawn::Document
   	y_position = cursor - 40
     image "#{Rails.root}/app/assets/images/cebolla.png", width: 50
 	bounding_box([0, y_position], :width => 200, :height => 200) do
-      text "UNIVERSIDAD SIMÓN BOLÍVAR", size: 8, style: :bold
+      text "    UNIVERSIDAD SIMÓN BOLÍVAR", size: 8, style: :bold
       text "Dirección de Finanzas - Oficina de Presupuesto", size: 8
-      text "Comisión de Planificación y Desarrollo", size: 8
+      text "    Comisión de Planificación y Desarrollo", size: 8
 	end
   end
   
