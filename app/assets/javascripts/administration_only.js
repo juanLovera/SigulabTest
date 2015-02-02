875c84ad3e70e37cba6364949e24533beb3af15a
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2015-02-02 19:11:10 -0430

    Javascript proyectos

--- app/assets/javascripts/administration_only.js
+++ app/assets/javascripts/administration_only.js
@@ -10,3 +10,56 @@ datepicking = function() {
 
 $(document).ready(datepicking);
 $(document).on('page:load', datepicking);
+
+
+function setSAECode() {
+
+	saeCode = "";
+
+	if ($('#project_admin').val()=="marquez")
+		saeCode = "1.05.03.11";
+
+	if ($('#project_admin').val()=="villa")
+		saeCode = "1.05.03.10";
+
+	$('#project_sae_code').val(saeCode);
+
+}
+
+function calculateAmount() {
+
+	if (isNaN(parseFloat($('#project_equipments').val()))) {
+		$('#project_equipments').val(0);
+    	equipments = 0;
+	} else {
+		equipments = parseFloat($('#project_equipments').val()) };
+
+	if (isNaN(parseFloat($('#project_services').val()))){
+		$('#project_services').val(0);
+    	services = 0;
+	} else {
+		services = parseFloat($('#project_services').val()) };
+
+	if (isNaN(parseFloat($('#project_infrastructure').val()))) {
+		$('#project_infrastructure').val(0);
+    	infrastructure = 0;
+	} else {
+		infrastructure = parseFloat($('#project_infrastructure').val()) };
+
+	if (isNaN(parseFloat($('#project_hhrr').val()))) {
+		$('#project_hhrr').val(0);
+    	hhrr = 0;
+	} else {
+		hhrr = parseFloat($('#project_hhrr').val()) };
+
+	if (isNaN(parseFloat($('#project_consumables').val()))) {
+		$('#project_consumables').val(0);
+    	consumables = 0;
+	} else {
+		consumables = parseFloat($('#project_consumables').val()) };
+
+	total = equipments+services+infrastructure+hhrr+consumables;
+
+	$('#project_amount').val(total);
+
+}
