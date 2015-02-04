1e4509504b95e4242c4ef470de5cb83b1ff822e6
nelsonsaturno <nelsonsaturno@gmail.com>
2015-02-04 12:13:59 -0430

    agregando donaciones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :donations
+
   resources :applications
 
   resources :loans
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204064358) do
+ActiveRecord::Schema.define(version: 20150204161542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -160,6 +160,17 @@ ActiveRecord::Schema.define(version: 20150204064358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "user_id"
+  end
+
+  create_table "donations", force: true do |t|
+    t.string   "nombreItem"
+    t.string   "marca"
+    t.string   "modelo"
+    t.string   "cantidad"
+    t.string   "unidadMedida"
+    t.string   "tipo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "equipment", force: true do |t|
