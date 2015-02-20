b40a3e79c54401e0af6c9610cd10ce145b528a0b
Juan <juanloverab@gmail.com>
2015-02-20 00:31:52 -0430

    Avances

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,4 +1,21 @@
 class Relation < ActiveRecord::Base
-  Tipo = ['Equipo', 'Material consumible', 'Servicio de mantenimiento de equipos', 'Licencias', 'Repuesto', 'Renovacion de licencias', 'Accesorio', 'Otro']
+  UnidadMedida = ['cm', 'm', 'in', 'pies']
+  UnidadMedida2 = ['gr','Kg','lb']
+  
+  validates_presence_of :marca, :message => " no puede estar en blanco" 
+  validates_presence_of :modelo, :message => " no puede estar en blanco"
+#  validates_numericality_of :alto, :greater_than => 0, :message => "debe ser un número mayor que 0"
+  validates_presence_of :alto, :message => "del Item no puede estar en blanco"
+  validates_presence_of :largo, :message => " no puede estar en blanco"
+#  validates_numericality_of :largo, :greater_than_or_equal_to => 0, :message => "debe ser un número mayor que 0"
+  validates_presence_of :ancho, :message => " no puede estar en blanco"
+#  validates_numericality_of :ancho, :greater_than_or_equal_to => 0, :message => "debe ser un número mayor que 0"
+  validates_presence_of :peso, :message => " no puede estar en blanco"
+#  validates_numericality_of :peso, :greater_than_or_equal_to => 0, :message => "debe ser un número mayor que 0"
+  validates_presence_of :dimensiones_alto, :message => " no puede estar en blanco" 
+  validates_presence_of :dimensiones_ancho, :message => " no puede estar en blanco"
+  validates_presence_of :dimensiones_largo, :message => " no puede estar en blanco" 
+  validates_presence_of :dimensiones_peso, :message => " no puede estar en blanco" 
+ 
   belongs_to :user
 end
