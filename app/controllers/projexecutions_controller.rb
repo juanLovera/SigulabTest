1a460c3deeac41fa15b90365635fd9c4c124b429
Jose Figueredo <josefigueredo@linuxmint.linuxmint-domain>
2015-03-01 23:53:30 -0430

    Bars

--- app/controllers/projexecutions_controller.rb
+++ app/controllers/projexecutions_controller.rb
@@ -33,6 +33,7 @@ class ProjexecutionsController < ApplicationController
        @commitment = Projcommitment.find(params[:cid])
        @projexecution.commitment_id = params[:cid]
        @projexecution.proyecto = @commitment.proj_id
+       @project = Project.find(@commitment.proj_id)
        @executed = Projexecution.where("commitment_id=?",params[:cid]).where("check_annulled=false").sum(:check_amount)       
      end
    end
