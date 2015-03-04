da7677355bd5c1288b37effc43faf15cee5a1026
tetercarrillo <mariaesthercarrillo.g@gmail.com>
2015-03-04 13:43:53 -0430

    inventario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150304015638) do
 
   # These are extensions that must be enabled in order to support this database
@@ -253,15 +252,6 @@ ActiveRecord::Schema.define(version: 20150304015638) do
 
   add_index "executions", ["commitment_id"], name: "index_executions_on_commitment_id", using: :btree
 
-  create_table "fecha_entregas", force: true do |t|
-    t.string   "ubicacion"
-    t.string   "fechaTope"
-    t.string   "condiciones"
-    t.string   "medida"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "incomes", force: true do |t|
     t.integer  "lab_id"
     t.float    "amount"
@@ -270,35 +260,17 @@ ActiveRecord::Schema.define(version: 20150304015638) do
     t.date     "date"
     t.string   "organism"
     t.string   "document"
+    t.string   "unit"
+    t.integer  "resource",             default: 0
+    t.string   "variation"
+    t.string   "resource_description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "sae_code"
     t.integer  "financing"
+    t.string   "doccode"
     t.string   "doc_code"
     t.date     "doc_date"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    t.boolean  "estado"
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 60a085223ec7d1d0d647919822d76302d8538ff1
->>>>>>> 24aebff7dd3c276b922c47683ebf210c8f390427
-=======
->>>>>>> 942491c2e378e9a80c481629ddaa0ce4146c96b9
-    t.string   "unit"
-    t.string   "variation"
-    t.string   "resource_description"
-    t.integer  "resource",             default: 0
-<<<<<<< HEAD
->>>>>>> 60a085223ec7d1d0d647919822d76302d8538ff1
->>>>>>> 71f9c1db455e211f8d1acd98c14e9f054be70686
-=======
->>>>>>> 942491c2e378e9a80c481629ddaa0ce4146c96b9
   end
 
   create_table "instruments", force: true do |t|
@@ -430,23 +402,8 @@ ActiveRecord::Schema.define(version: 20150304015638) do
     t.float    "furniture",      default: 0.0
     t.string   "other_desc"
     t.decimal  "other_amount",   default: 0.0
-    t.date     "annulled_date"
     t.string   "num_cuenta"
-<<<<<<< HEAD
     t.date     "annulled_date"
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> 60a085223ec7d1d0d647919822d76302d8538ff1
-    t.string   "observation"
-    t.integer  "banco"
->>>>>>> 24aebff7dd3c276b922c47683ebf210c8f390427
->>>>>>> 71f9c1db455e211f8d1acd98c14e9f054be70686
-=======
-    t.string   "observation"
-    t.integer  "banco"
->>>>>>> 942491c2e378e9a80c481629ddaa0ce4146c96b9
     t.string   "substitute"
   end
 
@@ -631,18 +588,15 @@ ActiveRecord::Schema.define(version: 20150304015638) do
     t.string   "location"
     t.string   "material"
     t.string   "bill"
+    t.boolean  "from_set"
     t.boolean  "investigation"
     t.boolean  "teaching"
     t.boolean  "extention"
     t.boolean  "management"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "cost"
-    t.string   "buy_order"
-    t.date     "adquisition_date"
-    t.boolean  "showable",         default: true
-    t.boolean  "from_set"
     t.string   "dependency"
+    t.boolean  "solicitados"
     t.string   "id2"
     t.string   "tipo"
     t.string   "origen"
@@ -680,5 +634,8 @@ ActiveRecord::Schema.define(version: 20150304015638) do
     t.boolean  "proy_responsible"
     t.boolean  "external"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
